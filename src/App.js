e6f910dd6a5afcf6543c2e74f431c27169bb25be
DavorPavits <163551802+DavorPavits@users.noreply.github.com>
2024-11-14 21:21:53 +0100

    Create Notes Stickers

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
 import './App.css';
+import notes from "./notes";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      {notes.map(e => (
+        <Note 
+          key={e.key}
+          title = {e.title}
+          content = {e.content}
+          />
+          ))};
+      <Footer></Footer>
     </div>
   );
 }
